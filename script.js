06c209e7ee5b43b9cd1f62c53b3e5f7e8a34adc8
JolieV2026 <jolie.vitale2026@gbwl.org>
2025-03-03 18:40:55 +0000

    Started Java Script

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+/* This is a MULTI-LINE comment 
+The code below is an example STATEMENT (instruction or command)
+console.log is a FUNCTION that displays infor in the console (output) area
+STATEMENTS end with a SEMICOLON (;)
+*/
+console.log("Welcome to JavaScript!!");
+
+// This is a SINGLE-LINE comment 
+
+// DECLARE A new VARIABLE 
+let pokemon;
+
+// ASSIGN a VALUE to the variable 
+// its like putting an item in the box
+pokemon ="Pikachu";
